49afad1e8730701054afa8c07ab25096c6e3509b
Ramen Guchhait <ramen2k10@gmail.com>
2023-07-21 15:48:30 +0530

    1. Enabled the swagger configuration
    2. Enabled the content negotiation

---
+++ source/advanced-rest-webservices/src/main/java/com/codify/advance/restservices/advancedrestwebservices/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.codify.advance.restservices.advancedrestwebservices;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+    
+}
